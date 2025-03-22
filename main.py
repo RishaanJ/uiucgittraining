806092bca9eb71eb088f617f8de629a1c24b4930
RishaanJ <rishaanjain@gmail.com>
2025-03-22 10:39:33 -0700

    Initial commit

---
+++ main.py
@@ -0,0 +1,10 @@
+def main():
+	name = input("What's your name: ")
+	if name == "satoshi":
+		print("hello satoshi!")
+	else:
+		print("goodbye " + name + "!")
+	
+
+if __name__ == '__main__':
+	main()
